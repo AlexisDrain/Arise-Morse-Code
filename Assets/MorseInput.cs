f978dc4d2bbaac1a67e3fe5d27acfeb6b6abb7d0
Alex Clay - Mudar Aljundi <alexanderclay9@gmail.com>
2017-08-17 04:11:28 -0400

    Hello World

---
+++ Assets/MorseInput.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorseInput : MonoBehaviour {
+
+
+	// also Input.GetButtonDown("SubmitPremature") for entering morse quickly
+
+	private float commandEndTimer;
+	private bool commandBeingEntered = false;
+
+	private float buttonHeldTime;
+	private bool buttonBeingHeldDown = false;
+
+	private string result = "dot";
+	
+	
+	private void Update () {
+
+		if (Input.GetButton("Jump") == true) {
+			commandBeingEntered = true;
+			// reset command timer
+			commandEndTimer = 0;
+		}
+
+		if (Input.GetButtonDown("Jump") == true) {
+			buttonBeingHeldDown = true;
+		}
+
+		if (Input.GetButtonUp("Jump") == true) {
+			buttonBeingHeldDown = false;
+			
+			if (buttonHeldTime < 0.2f) {
+				result = "dot";
+			} else {
+
+				result = "dash";
+			}
+
+			print(buttonHeldTime + " " + result);
+			buttonHeldTime = 0;
+			// reset command timer
+			commandEndTimer = 0;
+		}
+
+		if (buttonBeingHeldDown == true) {
+
+			buttonHeldTime += Time.deltaTime;
+		}
+		if (commandBeingEntered == true) {
+
+			commandEndTimer += Time.deltaTime;
+		}
+	}
+}
