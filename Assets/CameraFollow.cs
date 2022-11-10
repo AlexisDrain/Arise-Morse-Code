d050436769954d5309500f30ebda0c0743e7c6ee
Alexis <alexisdrain97@gmail.com>
2022-11-09 18:18:43 -0800

    imported CaberCSG

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class CameraFollow : MonoBehaviour
 {
     // test github
+    //public Vector2 offset = new Vector2(0, 0);
     public Vector2 cameraBounds = new Vector2(0, 0);
-    public Vector2 offset = new Vector2(0, 0);
 
     private Vector2 cameraCoords = new Vector2(0, 0);
     private Text levelnameText;
@@ -18,29 +18,25 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //print(GameManager.playerTransform.position);
+    void Update() {
 
-        Debug.DrawLine(-cameraBounds, cameraBounds);
+		if (GameManager.playerTransform.position.x > cameraBounds.x + transform.position.x) {
 
-		if (GameManager.playerTransform.position.x > cameraBounds.x + transform.position.x + offset.x) {
-
-            transform.position = new Vector3(transform.position.x + cameraBounds.x * 2, 0f, transform.position.y);
+			transform.position = new Vector3(transform.position.x + cameraBounds.x * 2, 0f, transform.position.z);
             cameraCoords = new Vector2(cameraCoords.x + 1, cameraCoords.y);
 
-        } else if (GameManager.playerTransform.position.x < -cameraBounds.x + transform.position.x - offset.x) {
+        } else if (GameManager.playerTransform.position.x < -cameraBounds.x + transform.position.x) {
 
-            transform.position = new Vector3(transform.position.x - cameraBounds.x * 2, 0f, transform.position.z);
+			transform.position = new Vector3(transform.position.x - cameraBounds.x * 2, 0f, transform.position.z);
             cameraCoords = new Vector2(cameraCoords.x - 1, cameraCoords.y);
 
          } else if (GameManager.playerTransform.position.z > cameraBounds.y + transform.position.z) {
-            print("up");
+
             transform.position = new Vector3(transform.position.x, 0f, transform.position.z + cameraBounds.y * 2);
             cameraCoords = new Vector2(cameraCoords.x, cameraCoords.y + 1);
 
         } else if (GameManager.playerTransform.position.z < -cameraBounds.y + transform.position.z) {
-			print("down");
+
 			transform.position = new Vector3(transform.position.x, 0f, transform.position.z - cameraBounds.y * 2);
             cameraCoords = new Vector2(cameraCoords.x, cameraCoords.y - 1);
         }
